50b66fffb5cfc0ba7a364e3b3c8fbbdb886ca956
Sophie9110 <95855606+Sophie9110@users.noreply.github.com>
2022-02-04 11:59:01 +0800

    emoji_convert
    第一次写函数没注意缩进.......

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-state_start=True
-state_stop=True
-while True:
-    information=input("> ")
-    if information=="help":
-        print("""
-start - to start the car
-stop - to stop the car
-quit - to exit""")
-    elif information=="start":
-        if state_start:
-            state_start=False
-            print("Car started...Ready to go")
-        else:
-            print("Car alredy has started")
-    elif information=="stop":
-        if state_stop:
-            state_stop=False
-            print("Car stopped.")
-        else:
-            print("Car alredy has stopped")
-    elif information=="quit":
-        break
-    else:
-        print("I don't understand that... ")
+def emoji(message):
+    words=message.split(" ")
+    emojis={
+      ":)": "😀",
+       ":(": "☹"
+    }
+    output = ""
+    for word in words:
+      output+=emojis.get(word,word)+" "
+    return output
 
+
+message = input(">>> ")
+output=emoji(message)
+print(output)
